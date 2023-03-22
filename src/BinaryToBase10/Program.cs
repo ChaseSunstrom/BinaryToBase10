e5f345e47e28a94e0fe36666ba7f118f72b819e1
Chase Sunstrom <Casunstrom+github@gmail.com>
2023-03-22 14:10:13 -0500

    Added the code for the project.

---
+++ src/BinaryToBase10/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace BinaryToBase10
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double add = 0;
+            Console.WriteLine("Enter a binary number: ");
+            string num = Console.ReadLine();
+    
+            num.Reverse();
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] == '1')
+                {
+                    add += Math.Pow(2, i);
+                }
+                
+                else if (num[i] != '1' && num[i] != '0')
+                {
+                    Console.WriteLine("Enter a valid binary number.");
+                    break;
+                }
+            }
+            
+            Console.WriteLine(add);
+        }
+    }
+}
